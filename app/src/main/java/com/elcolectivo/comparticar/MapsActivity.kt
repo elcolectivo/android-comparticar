ddacea0bb736e7a505c6078a42900668a91c6fd1
Juan Villada <jvillad@gmail.com>
2018-08-20 14:55:26 -0500

    Updated Marker location

--- app/src/main/java/com/elcolectivo/comparticar/MapsActivity.kt
+++ app/src/main/java/com/elcolectivo/comparticar/MapsActivity.kt
@@ -1,8 +1,7 @@
 package com.elcolectivo.comparticar
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-
+import android.support.v7.app.AppCompatActivity
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -36,8 +35,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap = googleMap
         
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val charles = LatLng(6.257292, -75.577014)
+        mMap.addMarker(MarkerOptions().position(charles).title("Reu Rizoma"))
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(charles, 15f))
     }
 }
